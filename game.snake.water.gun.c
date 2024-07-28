75fa97945ce186940f91dccb2f5f3decf60b4afd
whyhimanshu <vermahim402@gmail.com>
2024-07-28 20:39:25 +0530

    ProjectCommit2

--- game.snake.water.gun.c
+++ game.snake.water.gun.c
@@ -1,58 +1,74 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-int SnakeWaterGun(char you, char comp){
-    
-    if (you==comp){
+// SNAKE WATER GUN GAME
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int SnakeWaterGun(char you, char comp)
+{
+
+    if (you == comp)
+    {
         return 0;
     }
-    if(you=='s'&& comp=='w'){
+    if (you == 's' && comp == 'w')
+    {
         return 1;
     }
-    else if(comp=='w' && you =='s'){
+    else if (comp == 'w' && you == 's')
+    {
         return -1;
     }
-    if(you=='s'&& comp=='g'){
+    if (you == 's' && comp == 'g')
+    {
         return -1;
     }
-    else if(you=='g'&& comp=='s'){
+    else if (you == 'g' && comp == 's')
+    {
         return 1;
     }
-    if(you=='w'&&comp=='g'){
+    if (you == 'w' && comp == 'g')
+    {
         return 1;
     }
-    else if(you=='g'&&comp=='w'){
+    else if (you == 'g' && comp == 'w')
+    {
         return -1;
     }
 }
-int main(){
-    char you,comp;
+int main()
+{
+    char you, comp;
     int number;
     srand(time(0));
-    number =rand()%100 + 1;  //GENERATES A number BETWEEN 1 AND 100
-    if(number<33 && number>0){
-        comp ='s';
+    number = rand() % 100 + 1; // GENERATES A number BETWEEN 1 AND 100
+    if (number < 33 && number > 0)
+    {
+        comp = 's';
     }
-    else if(number>33 && number <66){
+    else if (number > 33 && number < 66)
+    {
         comp = 'w';
     }
-    else{
+    else
+    {
         comp = 'g';
     }
 
     printf("Enter 's' for snake, 'w' for water and 'g' for gun :\n");
-    scanf("%c",&you);
-    printf("You chose %c and computer chose %c\n",you,comp);
-    int result = SnakeWaterGun(you,comp);
-    if(result==0){
+    scanf("%c", &you);
+    printf("You chose %c and computer chose %c\n", you, comp);
+    int result = SnakeWaterGun(you, comp);
+    if (result == 0)
+    {
         printf("Game Draw 🤯😂\n");
     }
-    else if(result ==1){
+    else if (result == 1)
+    {
         printf("You won the game, congratulations😎\n");
     }
-    else {
+    else
+    {
         printf("sorry you lost:/\n");
     }
 
-return 0;
+    return 0;
 }
--- game_random_nogen.c
+++ game_random_nogen.c
@@ -1,27 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+//RANDOM NUMBER GENERATOR GAME
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main()
 {
-    int number,enterednum;
-    int count=1;
+    int number, enterednum;
+    int count = 1;
     srand(time(0));
-    number = rand()%100+1;  /*generates a number between 1aand 100*/
-   
-    
-    while(enterednum!=number){
-    printf("Enter the number which you want (less than 100)😎 : \n");
-    scanf("%d",&enterednum);
-    if(enterednum>number){
-        printf("LESS NUMBER PLEASE\n");
+    number = rand() % 100 + 1; /*generates a number between 1aand 100*/
+
+    while (enterednum != number)
+    {
+        printf("Enter the number which you want (less than 100)😎 : \n");
+        scanf("%d", &enterednum);
+        if (enterednum > number)
+        {
+            printf("LESS NUMBER PLEASE\n");
+        }
+        else if (number > enterednum)
+        {
+            printf("BIGGER NUMBER PLEASE\n");
+        }
+        else
+        {
+            printf("Congratulations🥳 you predicted number in %d times and the number is %d\n", count, number);
+        }
+        count++;
     }
-    else if(number>enterednum){
-        printf("BIGGER NUMBER PLEASE\n");
-    }
-    else{
-        printf("Congratulations🥳 you predicted number in %d times and the number is %d\n",count,number);
-    }
-    count++;
-        }   
-return 0;
+    return 0;
 }
